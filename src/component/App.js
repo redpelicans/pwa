49578b2cf020ac8ee26955733a8d7e856126367a
abarriel <abarriel@student.42.fr>
2017-08-10 18:18:07 +0200

    redux implementation

--- src/component/App.js
+++ src/component/App.js
@@ -7,40 +7,37 @@ import { addTodo, deleteTodo, updateTodo } from '../actions/todos';
 const createTodoListItem = (todo, handleDeleteButton, handleModif) =>  
   <li key={`li_${todo._id}`}>
     <div className="view" >
-        <input className="toggle" type="checkbox" defaultChecked={todo.completed} />
-        <input type="text" className="labels" defaultValue={todo.title} onKeyPress={handleModif(todo)} /> 
-        <button className="destroy" onClick={() => handleDeleteButton(todo)}/>
+      <input className="toggle" type="checkbox" defaultChecked={todo.completed} />
+      <input type="text" className="labels" defaultValue={todo.title} onKeyPress={handleModif(todo)} /> 
+      <button className="destroy" onClick={() => handleDeleteButton(todo)}/>
     </div>
     <input id={`input_${todo._id}`} className="edit" defaultValue={todo.title} />
-   </li>; 
+  </li>; 
 
 class App extends Component {
 
   handleKeyPress = ({ key, target: { value }}) => {
-    if (key === 'Enter') {
-      const { addTodo } = this.props;
-      addTodo(value);
-    };
+   if (key === 'Enter') {
+    const { addTodo } = this.props;
+    addTodo(value);
+   };
   };
 
   handleDeleteButton = (todo) => {
-    console.log('habdleDeleteButton');
-    const { deleteTodo } = this.props;
-    deleteTodo(todo);
+   const { deleteTodo } = this.props;
+   deleteTodo(todo);
   };
 
-    handleModif = (todo) => ({ key, target: { value }}) => {
-    if (key === 'Enter') {
-      const { updateTodo } = this.props;
-      updateTodo(value, todo);
-    };
-    console.log(value);
+  handleModif = (todo) => ({ key, target: { value }}) => {
+   if (key === 'Enter') {
+     const { updateTodo } = this.props;
+     updateTodo(value, todo);
+   };
   };
 
   render() {
     const { todos } = this.props;
-    if(!todos) return null;
-    console.log(todos);
+    if (!todos) return null;
     return (
     <div className="todoapp">
       <header className="header">
@@ -65,4 +62,5 @@ const mapDispatchToProps = {
   deleteTodo,
   updateTodo,
 };
+
 export default connect(mapStateToProps, mapDispatchToProps)(App);
